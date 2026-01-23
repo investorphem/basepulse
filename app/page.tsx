6f484e7f1c7be3f1ec062799023048aed433b937
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-23 10:07:12 +0100

    Create initial Page component with metadata

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <html>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="/signal.png" />
+        <meta property="fc:frame:button:1" content="⚡ Signal" />
+        <meta property="fc:frame:post_url" content="/api/signal" />
+      </head>
+      <body />
+    </html>
+  );
+}
