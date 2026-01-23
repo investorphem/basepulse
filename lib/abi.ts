b942a0d0816c81e4e0e5c2eda1bb88f5e77343f0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-23 10:04:35 +0100

    Add EXECUTOR_ABI for execute function

---
+++ lib/abi.ts
@@ -0,0 +1,14 @@
+export const EXECUTOR_ABI = [
+  {
+    name: "execute",
+    type: "function",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "targets",
+        type: "address[]",
+      },
+    ],
+    outputs: [],
+  },
+];
