084725d0daa48e0b498ce076336bff5a1f89c98d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-23 14:36:27 +0100

    Rename function from executeAll to executeRandom

--- lib/executor.ts
+++ lib/executor.ts
@@ -1,11 +1,11 @@
-export const EXECUTOR_ADDRESS = "0x68Ec3260171F3609bc68Dff81A14fd168A7bf1A1";
+export const EXECUTOR_ADDRESS = "0x68Ec3260171F3609bc68Dff81A14fd168A7bf1A1" as const;
 
 export const EXECUTOR_ABI = [
   {
     inputs: [{ internalType: "address[]", name: "targets", type: "address[]" }],
-    name: "executeAll",
+    name: "executeRandom", // Changed from executeAll
     outputs: [],
     stateMutability: "nonpayable",
     type: "function"
   }
-];
+] as const;
